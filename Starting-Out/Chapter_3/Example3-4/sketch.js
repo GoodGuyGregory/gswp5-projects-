40526dc212e12a3be86bc6b13800f14a3aebd708
GoodGuyGregory <greg.witt625@gmail.com>
2020-02-22 18:51:05 -0500

    adds examples from chapter 3

---
+++ Starting-Out/Chapter_3/Example3-4/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(480,120);
+}
+
+function draw() {
+  background(204);
+  // line(50,80,500,130);
+  // Quadratics(x1,y1,x2,y2,x3,y3,x4,y4)
+  quad(158, 55,199,14,392,66,351,107);
+  // triangles(x1,y1,x2,y2,x3,y3)
+  triangle(347,54,392,9,392,66);
+  triangle(158,55,290,91,290,112);
+}
