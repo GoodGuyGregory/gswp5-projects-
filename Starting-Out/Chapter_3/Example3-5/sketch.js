0bc389c562b9551adc30018b311dafa676ba252b
GoodGuyGregory <greg.witt625@gmail.com>
2021-05-10 17:54:52 -0400

    adds Example3-5

---
+++ Starting-Out/Chapter_3/Example3-5/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(1980, 1020);
+}
+
+function draw() {
+    //  Create a Rectangle:
+    background(204);
+    rect(180, 60, 420, 440);
+    rect(380, 160, 420, 140);
+    rect(480, 600, 520, 340);
+    rect(680, 680, 620, 140);
+    rect(1080, 800, 320, 440);
+    rect(1480, 200, 420, 1740);
+    rect(780, 300, 320, 440);
+
+
+}
