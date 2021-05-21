270d15818a46968e14b62f8b1524eaa88840ca73
GoodGuyGregory <greg.witt625@gmail.com>
2021-05-21 16:25:18 -0400

    adds Example 4-5

---
+++ 4-Variables/Example4-5/sketch.js
@@ -0,0 +1,29 @@
+//  Variables:
+
+
+function setup() {
+    createCanvas(1980, 1020);
+    strokeWeight(8);
+}
+
+function draw() {
+    background(204);
+    // Without a Loop:
+    line(20, 40, 80, 80);
+    line(80, 40, 140, 80);
+    line(140, 40, 200, 80);
+    line(200, 40, 260, 80);
+    line(260, 40, 320, 80);
+    line(320, 40, 380, 80);
+    line(380, 40, 440, 80);
+
+
+
+
+
+
+
+
+
+
+}
