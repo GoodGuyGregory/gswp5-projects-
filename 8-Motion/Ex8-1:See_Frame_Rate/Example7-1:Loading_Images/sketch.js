84fba89c12537545c2567399e65dfb31ff9d2d39
GoodGuyGregory <greg.witt625@gmail.com>
2021-06-25 17:12:28 -0400

    adds chapter 8 exmaples

---
+++ 8-Motion/Ex8-1:See_Frame_Rate/Example7-1:Loading_Images/sketch.js
@@ -0,0 +1,14 @@
+//  Variables:
+
+
+function setup() {
+    createCanvas(1980, 1020);
+    frameRate(0.5);
+
+}
+
+function draw() {
+    //  display the framerate of the program
+    var fr = frameRate();
+    print(fr);
+}
---
+++ 8-Motion/Ex8-1:See_Frame_Rate/sketch.js
@@ -0,0 +1,13 @@
+//  Variables:
+
+
+function setup() {
+    createCanvas(1980, 1020);
+
+}
+
+function draw() {
+    //  display the framerate of the program
+    var fr = frameRate();
+    print(fr);
+}
