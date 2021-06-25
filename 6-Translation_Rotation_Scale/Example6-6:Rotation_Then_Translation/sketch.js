79d5cb27084c891cf58c4b3e2609c2c5537e6697
GoodGuyGregory <greg.witt625@gmail.com>
2021-06-25 13:59:52 -0400

    adds name and code for Hypnotize Example

--- 6-Translation_Rotation_Scale/Example6-6:Rotation_Then_Translation/sketch.js
+++ 6-Translation_Rotation_Scale/Example6-6:Rotation_Then_Translation/sketch.js
@@ -3,17 +3,13 @@ var angle = 0.0;
 
 function setup() {
     createCanvas(1980, 1020);
-
+    background(204);
 
 }
 
 function draw() {
-    background(204);
-    stroke(0);
     rotate(angle);
     translate(mouseX, mouseY);
-    rect(-15, -15, 90, 90);
-    fill(0);
     rect(-15, -15, 90, 90);
     angle += 0.5;
 }
