2ee039ea7ada4929b15e9d46f3c14bddccc505e6
GoodGuyGregory <greg.witt625@gmail.com>
2021-07-05 11:04:21 -0400

    adds working sine Spine and sine values

--- 8-Motion/Ex8-13:Sine_Wave_Movement/sketch.js
+++ 8-Motion/Ex8-13:Sine_Wave_Movement/sketch.js
@@ -20,11 +20,34 @@ function draw() {
 
     //  iterate through these to add them to the screen
     let boundary = width - 100
-    for (let x = 80; x < boundary; x += 40) {
-        ellipse(x, offset, 80, 80);
+    // for (let x = 80; x < boundary; x += 40) {
+    //     ellipse(x, offset, 80, 80);
+    // }
 
+    //  Check 
+    let x = 80;
+    let count = 0;
+    let angleDelta = 0.4;
+    while (count <= boundary) {
+        fill(color(120, 255, 120));
+        if (count == 0 || count == boundary) {
+            var initialAngle = offset + sin(angle) * scalar;
+            // fill(color(255, 0, 0));
+            ellipse(x, initialAngle, 80, 80);
+        }
+        else {
+            var deltaAngle = offset + sin(angle + angleDelta) * scalar;
+            // fill(color(255, 255, 255));
+            ellipse(x, deltaAngle, 80, 80);
+            angleDelta += 0.4;
 
+            // fill(color(255, 255, 255));
+            // ellipse(x, y2, 80, 80);
+        }
+        x += 40;
+        count += 40;
     }
+
     // ellipse(80, y1, 80, 80);
     // ellipse(120, y2, 80, 80);
     // ellipse(160, y3, 80, 80);
