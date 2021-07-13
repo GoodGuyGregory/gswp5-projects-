dea8a4355c735d4cc28c87343f37d1fa5ed262ba
GoodGuyGregory <greg.witt625@gmail.com>
2021-07-13 09:21:51 -0400

    initial commit for chapter 9

---
+++ 9-Functions/Ex9-3:Draw_the_Owl/sketch.js
@@ -0,0 +1,27 @@
+//  Variables:
+
+
+
+function setup() {
+    createCanvas(1980, 1020);
+
+}
+
+function draw() {
+    background(204);
+    //  translates the origin from 0,0 to 110,110
+    translate(110, 110);
+    stroke(0);
+    strokeWeight(70);
+    //  Body of the owl
+    line(0, -35, 0, -65);
+    noStroke();
+    fill(204);
+    ellipse(-17.5, -65, 35, 35); // left eye
+    ellipse(17.5, -65, 35, 35);  // right eye
+    arc(0, -65, 70, 70, 0, PI); // chin
+    fill(0);
+    ellipse(-14, -65, 8, 8); // left eye
+    ellipse(14, -65, 8, 8); // right eye
+    quad(0, -58, 4, -51, 0, -44, -4, -51);
+}
